2aefc63431dc0639d48872fde939352d1ed8114b
nimish-ux <nimishnerlekarwork@gmail.com>
2024-10-03 15:18:19 +0530

    updates

--- src/app/constant/commonConstant.ts
+++ src/app/constant/commonConstant.ts
@@ -6,6 +6,10 @@ import PDImage2 from "../assets/PDImg2.jpg";
 import GDImage1 from "../assets/GDimg1.jpg";
 import GDImage2 from "../assets/GDimg2.jpg";
 import profileImg2 from "../assets/profileimage2.png";
+import 1Huddle from "../assets/1huddle.png";
+import frame3d from "../assets/frame3d.png";
+import router from "../assets/router.png";
+
 
 
 
@@ -14,7 +18,7 @@ export const jobTitles = ["UI UX Designer"," Product Designer", "Visual Designer
 export const tabLabels = [
   {
     id: 1,
-    label: "Product Design",
+    label: "UI/UX Design",
     items: [
       {
         id: 1,
@@ -24,25 +28,46 @@ export const tabLabels = [
       },
       {
         id: 2,
-        label:"2021 Portfolio - PD",
-        image: PDImage2,
-        docURL: "https://www.behance.net/gallery/127608911/Portfolio-2021-Product-Design",
+        label: "1Huddle",
+        image: "1Huddle",
+        docURL: "https://1huddle.co/",
       },
       {
         id: 3,
-        label: "1Huddle",
-        image: "https://via.placeholder.com/150",
-      },
-      {
-        id: 4,
         label: "UI Explorations",
-        image: "https://via.placeholder.com/150",
+        image: "1Huddle",
+        docURL: "https://1huddle.co/",
       },
     ],
   },
   {
     id: 2,
-    label: "Graphic Design",
+    label: "Industrial Design",
+    items: [
+      {
+        id: 1,
+        label:"2021 Portfolio - PD",
+        image: PDImage2,
+        docURL: "https://www.behance.net/gallery/127608911/Portfolio-2021-Product-Design",
+      },
+      {
+        id: 2,
+        label: "Graduation Project - TI Cycles of India",
+        image: PDImage1,
+        docURL: "https://drive.google.com/file/d/1mQ4gpm3CIfwYzCPVp917Bh77sgGt_S3O/view?usp=drive_link",
+      },
+      {
+        id: 3,
+        label: "CMF explorations - Wifi Router (2021)",
+        image: router,
+        docURL: "https://drive.google.com/file/d/1Y9taIwz97gdBhO9Ogmn7mWbkgmZH7wa4/view?usp=sharing",
+      },
+
+    ],
+  },
+  {
+    id: 3,
+    label: "Art & Graphic Design",
     items: [
       {
         id: 1,
@@ -52,46 +77,34 @@ export const tabLabels = [
       },
       {
         id: 2,
+        label: "Digital Art 2024",
+        image: "https://via.placeholder.com/150",
+      },
+      {
+        id: 3,
         label: "3d Rendering 2021 explorations",
         image: GDImage2,
         docURL: "https://www.behance.net/gallery/117146493/3d-Modeling-and-Rendering-explorations",
       },
-      {
-        id: 3,
-        label: "Digital Art 2024",
-        image: "https://via.placeholder.com/150",
-      },
-    ],
-  },
-  {
-    id: 3,
-    label: "3D Printing",
-    items: [
-      {
-        id: 1,
-        label:"2021 Portfolio - PD",
-        image: PDImage2,
-        docURL: "https://www.behance.net/gallery/127608911/Portfolio-2021-Product-Design",
 
-      },
     ],
   },
   {
     id: 4,
-    label: "Blog",
+    label: "3D Printing & Prototyping",
     items: [
       {
         id: 1,
-        label:"2021 Portfolio - PD",
-        image: PDImage2,
-        docURL: "https://www.behance.net/gallery/127608911/Portfolio-2021-Product-Design",
+        label:"Vintage Frame Decoration (Wall Mount)",
+        image: frame3d,
+        docURL: "https://www.printables.com/model/1026779-vintage-frame-shelf",
 
       },
     ],
   },
   {
     id: 5,
-    label: "About me",
+    label: "Resources",
     items: [
       {
         id: 1,
